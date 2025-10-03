4df4078ae9132726d1e0d6e3bc8304fcc8a06224
Pranali Jadhav <jadhavpranali481@gmail.com>
2025-10-03 12:51:28 +0530

    commited from vs

--- Demo1.java
+++
@@ -1,8 +0,0 @@
-package Demo;
-
-public class Demo1 {
-
-    public static void main(String[] args) {
-        System.err.println("hiiiiiiiii");
-    }
-}
---
+++ src/Exception/StackOverflowError.java
@@ -0,0 +1,11 @@
+package Exception;
+//this is error
+public class StackOverflowError {
+
+	public static void main(String[] args) {
+		for(;;) {
+			System.out.println("hello");
+		}
+	}
+
+}
